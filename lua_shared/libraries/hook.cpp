f0583dffba3a8a48bf6f3753666063abdeaed675
Tim Schumacher <timschumi@gmx.de>
2022-10-24 14:48:09 +0200

    lua_shared: Correct the argument indices in hook.Add

--- lua_shared/libraries/hook.cpp
+++ lua_shared/libraries/hook.cpp
@@ -3,9 +3,9 @@
 
 int CLuaBase::lua$hook_add()
 {
-    char const* event_name = lua_tostring(lua_state, -3);
-    char const* identifier = lua_tostring(lua_state, -2);
-    lua_CFunction func = lua_tocfunction(lua_state, -1);
+    char const* event_name = lua_tostring(lua_state, 1);
+    char const* identifier = lua_tostring(lua_state, 2);
+    lua_CFunction func = lua_tocfunction(lua_state, 3);
 
     // FIXME: Implement hooks.
     printf("FIXME: hook.Add('%s', '%s', %p) is not implemented.\n", event_name, identifier, func);
