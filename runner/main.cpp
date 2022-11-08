7650adbe7db20e3756cfb925e81d750e477b06c2
Tim Schumacher <timschumi@gmx.de>
2022-11-08 22:56:41 +0100

    runner: Allow changing directory before running the script

--- runner/main.cpp
+++ runner/main.cpp
@@ -1,17 +1,35 @@
 #include <CLuaBase.h>
 #include <GarrysMod/Lua/Interface.h>
 #include <cstdio>
+#include <cstring>
+#include <filesystem>
+#include <unistd.h>
 
 int main(int argc, char const** argv)
 {
-    CLuaBase lua_base;
+    char* directory_from_env = getenv("GMOD_DIR");
+    std::string original_directory = std::filesystem::current_path().generic_string();
+
+    if (directory_from_env) {
+        if (chdir(directory_from_env) < 0) {
+            fprintf(stderr, "Failed to change directory to '%s': %s\n", directory_from_env, strerror(errno));
+            return 1;
+        }
+    }
 
     if (argc <= 1) {
         fprintf(stderr, "error: Need a script to run.\n");
         return 1;
     }
 
-    switch (lua_base.load_and_run_file(argv[1])) {
+    std::string script_path = argv[1];
+
+    if (!script_path.starts_with('/'))
+        script_path = original_directory + "/" + script_path;
+
+    CLuaBase lua_base;
+
+    switch (lua_base.load_and_run_file(script_path.c_str())) {
     case CLuaBase::Success: {
         int return_value = static_cast<int>(lua_base.CheckNumber(-1));
         lua_base.run_event_loop();
