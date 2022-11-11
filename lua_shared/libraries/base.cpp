970bdc3d05edfd898cc031b9cb1d7366281762e1
Tim Schumacher <timschumi@gmx.de>
2022-11-12 00:00:11 +0100

    lua_shared: Combine all files for the `base` module into one

--- lua_shared/libraries/base.cpp
+++ lua_shared/libraries/base.cpp
@@ -21,6 +21,105 @@ int CLuaBase::lua$assert()
     return 2 + number_of_varargs;
 }
 
+// https://wiki.facepunch.com/gmod/Global.Color
+int CLuaBase::lua$meta$Color_eq()
+{
+#define COMPARE_INDEX(name)                     \
+    lua_pushstring(lua_state, name);            \
+    lua_gettable(lua_state, 1);                 \
+    lua_pushstring(lua_state, name);            \
+    lua_gettable(lua_state, 2);                 \
+    if (lua_rawequal(lua_state, -1, -2) == 0) { \
+        lua_pushboolean(lua_state, false);      \
+        return 1;                               \
+    }
+    COMPARE_INDEX("r")
+    COMPARE_INDEX("g")
+    COMPARE_INDEX("b")
+    COMPARE_INDEX("a")
+
+    lua_pushboolean(lua_state, true);
+    return 1;
+#undef COMPARE_INDEX
+}
+
+// https://wiki.facepunch.com/gmod/Global.Color
+int CLuaBase::lua$Color()
+{
+    int number_of_arguments = lua_gettop(lua_state);
+
+    lua_createtable(lua_state, 0, 4);
+
+    lua_pushstring(lua_state, "r");
+    lua_pushvalue(lua_state, 1);
+    lua_settable(lua_state, -3);
+
+    lua_pushstring(lua_state, "g");
+    lua_pushvalue(lua_state, 2);
+    lua_settable(lua_state, -3);
+
+    lua_pushstring(lua_state, "b");
+    lua_pushvalue(lua_state, 3);
+    lua_settable(lua_state, -3);
+
+    lua_pushstring(lua_state, "a");
+    if (number_of_arguments >= 4)
+        lua_pushvalue(lua_state, 4);
+    else
+        lua_pushnumber(lua_state, 255);
+    lua_settable(lua_state, -3);
+
+    luaL_setmetatable(lua_state, "Color");
+
+    return 1;
+}
+
+// https://wiki.facepunch.com/gmod/ConVar:GetBool
+int CLuaBase::lua$meta$ConVar_GetBool()
+{
+    auto convar = static_cast<ConVar**>(lua_touserdata(lua_state, 1));
+
+    // If the value is numeric and not 0, the result will be true. Otherwise, the result will be false.
+    lua_pushboolean(lua_state, strtod((*convar)->value.c_str(), nullptr) != 0);
+
+    return 1;
+}
+
+// https://wiki.facepunch.com/gmod/ConVar:SetBool
+int CLuaBase::lua$meta$ConVar_SetBool()
+{
+    auto convar = static_cast<ConVar**>(lua_touserdata(lua_state, 1));
+    auto value = lua_toboolean(lua_state, 2);
+
+    (*convar)->value = value ? "1" : "0";
+
+    return 0;
+}
+
+// https://wiki.facepunch.com/gmod/Global.CreateConVar
+int CLuaBase::lua$CreateConVar()
+{
+    int number_of_arguments = lua_gettop(lua_state);
+    std::string name = lua_tostring(lua_state, 1);
+    std::string value = lua_tostring(lua_state, 2);
+    long flags = (number_of_arguments >= 3) ? static_cast<long>(lua_tonumber(lua_state, 3)) : FCVAR_NONE;
+    auto helptext = (number_of_arguments >= 4) ? std::optional<std::string> { lua_tostring(lua_state, 4) } : std::nullopt;
+    auto min = (number_of_arguments >= 5) ? std::optional<double> { lua_tonumber(lua_state, 5) } : std::nullopt;
+    auto max = (number_of_arguments >= 6) ? std::optional<double> { lua_tonumber(lua_state, 6) } : std::nullopt;
+
+    if (!convars.contains(name)) {
+        convars[name] = { value, flags, helptext, min, max };
+    }
+
+    ConVar* convar = &convars[name];
+
+    auto userdata = static_cast<ConVar**>(lua_newuserdata(lua_state, sizeof(void*)));
+    *userdata = convar;
+    luaL_setmetatable(lua_state, "ConVar");
+
+    return 1;
+}
+
 // https://wiki.facepunch.com/gmod/Global.CurTime
 int CLuaBase::lua$CurTime()
 {
@@ -54,6 +153,25 @@ int CLuaBase::lua$ErrorNoHaltWithStack()
     printf("\n");
 
     return 0;
+}
+
+// https://wiki.facepunch.com/gmod/Global.GetConVar
+int CLuaBase::lua$GetConVar()
+{
+    std::string name = lua_tostring(lua_state, 1);
+
+    if (!convars.contains(name)) {
+        lua_pushnil(lua_state);
+        return 1;
+    }
+
+    ConVar* convar = &convars[name];
+
+    auto userdata = static_cast<ConVar**>(lua_newuserdata(lua_state, sizeof(void*)));
+    *userdata = convar;
+    luaL_setmetatable(lua_state, "ConVar");
+
+    return 1;
 }
 
 // https://wiki.facepunch.com/gmod/Global.getfenv
