f776f7636becc9d8595c1043cef73b7fb2fe0713
Tim Schumacher <timschumi@gmx.de>
2022-11-14 13:42:16 +0100

    test: Report end result only after all tests have run

--- test.lua
+++ test.lua
@@ -1,17 +1,22 @@
+runner.return_value = 1
 failed_tests = {}
 SERVER = true
 GetConVar("gluatest_enable"):SetBool(true)
 
-hook.Add("GLuaTest_LoggedTestFailure", "record_end_result", function(errInfo)
+hook.Add("GLuaTest_LoggedTestFailure", "record_test_failures", function(errInfo)
     table.insert(failed_tests, errInfo)
 end)
 
-hook.Run("Tick")
+hook.Add("GLuaTest_Finished", "report_end_result", function()
+    if #failed_tests == 0 then
+        runner.return_value = 0
+        return
+    end
 
-if #failed_tests > 0 then
     print("Failed " .. #failed_tests .. " tests:")
     for _, info in ipairs(failed_tests) do
         print(" - " .. info.sourceFile .. ":" .. info.lineNumber .. ": " .. info.reason)
     end
-    return 1
-end
+end)
+
+hook.Run("Tick")
